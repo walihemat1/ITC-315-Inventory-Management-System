792da3c1bfd45273bcf6a242fc9acdcc2c4a3f75
Alisina Nazari <cena.nazari980@gmail.com>
2025-11-24 09:56:08 +0430

    product controllers for get, get all, and create

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,18 +1,32 @@
 import Product from "../models/productModel";
 
+// Create a product
 export const createProduct = async (req, res) => {
   try {
-    const { productName, description, quantity } = req.body;
     const product = await Product.create(req.body);
-  } catch (error) {}
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 };
 
 // get all products
 export const getProducts = async () => {};
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 
-// get a single a product
+// get a single a product By Id
 export const getProduct = async () => {};
-
+  try {
+    const {id} = req.params;
+    const product = await product.findById(id);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
 export const updateProduct = async () => {};
 
 export const deleteProduct = async () => {};
