602d39b62c99668e5f41050f3dc7f0906fdc01f0
walihemat1 <ahmadwalihemat67@gmail.com>
2025-11-25 14:55:13 +0430

    add user routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,3 +1,8 @@
-import { register } from "../controllers/authController";
+import express from "express";
+import { register, login, logout } from "../controllers/authController";
+
+const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
