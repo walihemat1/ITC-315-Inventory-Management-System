bb50ca39d1ea24b6f8d7c12c775fccb63f6eb3f8
walihemat1 <ahmadwalihemat67@gmail.com>
2025-11-25 14:42:37 +0430

    add product Schema

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,30 @@
-const express = require("express");
-const Product = require("./models.productModel.js");
-const mongoose = require("mongoose");
-const productRoute = require ("./routes/productRoutes.js");
+import express from "express";
+import env from "dotenv";
+import mongoose from "mongoose";
+
+import productRoute from "./routes/productRoutes.js";
+
+env.config();
+
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //routes
-app.use('/api/products', productRoute);
+app.use("/api/products", productRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("DB connected successfully");
+      console.log(`App is listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection Error: ", err);
+    process.exit(1);
+  });
