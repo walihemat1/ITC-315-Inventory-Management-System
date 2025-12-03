61079089ed7d68b8bc9e9171f7ce6cba4f086633
Alisina Nazari <cena.nazari980@gmail.com>
2025-12-03 11:29:30 +0430

    product list and filter

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import env from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+
 
 import productRoute from "./routes/productRoutes.js";
 import categoryRoute from "./routes/categoryRouter.js";
@@ -28,6 +30,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+// OR to allow only your frontend:
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 //routes
 app.use("/api/products", productRoute);
